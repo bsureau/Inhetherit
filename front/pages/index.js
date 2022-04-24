e8600d84538f320f6179449377e61e411ae1e988
bsureau <sureau.benjamin@gmail.com>
2022-04-24 18:19:53 +0000

    feat: approve, get allowance and transferFrom with dummy ERC20 (WIP)

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,6 +1,62 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ethers } from "ethers";
+
+
+async function approve() {
+
+  const dercAddress = "0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1";
+  const dercAbi = [
+    "function approve(address _spender, uint256 _value) public returns (bool success)"
+  ];
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+
+  const dercContract = new ethers.Contract(dercAddress, dercAbi, signer);
+
+  const tx = await dercContract.approve(dercAddress, ethers.utils.parseUnits("2", 18));
+
+  await tx.wait(1);
+  console.log(tx);
+}
+
+async function getAllowance() {
+
+  const dercAddress = "0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1";
+  const dercAbi = [
+    "function allowance(address _owner, address _spender) public view returns (uint256 remaining)"
+  ];
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  const dercContract = new ethers.Contract(dercAddress, dercAbi, provider);
+  const signer = provider.getSigner();
+  const address = await signer.getAddress();
+  const allowance = await dercContract.allowance(address, dercAddress);
+  console.log("allowance: ", allowance.toString());
+}
+
+async function transfer() {
+
+  const dercAddress = "0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1";
+  const dercAbi = [
+    "function balanceOf(address account) public view returns (uint256 balance)",
+    "function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)"
+  ];
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const dercContract = new ethers.Contract(dercAddress, dercAbi, signer);
+  const address = await signer.getAddress();
+  console.log('address: ', address);
+  const balance = await dercContract.balanceOf(address);
+  console.log("balance: ", balance.toString());
+  const tx = await dercContract.transferFrom(address, "0xbfDa4dC845e078f2C8239a2c1954cd5b49ddE378", balance.toString());
+  tx.wait(1);
+  console.log("tx: ", tx.hash);
+}
 
 export default function Home() {
   return (
@@ -15,6 +71,18 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <button onClick={approve}>
+          APPROVE
+        </button>
+
+        <button onClick={getAllowance}>
+          GET ALLOWANCE
+        </button>
+
+        <button onClick={transfer}>
+          TRANSFER
+        </button>
 
         <p className={styles.description}>
           Get started by editing{' '}
