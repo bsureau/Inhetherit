00e10540f4fb976c9e43804587dc5680768d808f
Romain Quilliot <romain.addweb@gmail.com>
2022-05-04 18:49:01 +0200

    refactor: remove useless import

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import { NextUIProvider } from '@nextui-org/react';
 import React from 'react';
-import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from '../store'
 
