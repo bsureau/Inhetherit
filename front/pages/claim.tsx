f16f2e12413fdfd02f9e57fe54ca983a35ff84d4
bsureau <sureau.benjamin@gmail.com>
2022-05-08 12:41:48 +0000

    feat(front): build claim page (WIP)

--- front/pages/claim.tsx
+++ front/pages/claim.tsx
@@ -1,17 +1,112 @@
-import Head from 'next/head'
+import { useEffect, useState } from "react";
+import Head from 'next/head';
+
+import {
+  Container,
+  Row,
+  Col,
+  Spacer
+} from "@nextui-org/react";
+
+import {
+  TopBar,
+  Sidebar
+} from './components';
+
+import { connectWallet, getWallet } from "../utils/metamask";
+import { getWill } from "../utils/willContract";
+import { useUser } from "../context/user";
+import { useWill } from "../context/will";
+
+const styles: any = {
+  container: {
+    minHeight: '100vh',
+    minWidth: '100vw',
+    padding: '0',
+    zIndex: "-10"
+  },
+  column: {
+    marginLeft: "280px",
+    paddingTop: "5.3rem",
+    background: "#fefefe",
+    minHeight: "100vh",
+    textAlign: "center",
+    width: "100%",
+  },
+};
 
 export default function Claim() {
+  const { user, setUser } = useUser();
+  const { will, setWill } = useWill();
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(function () {
+    getWallet(window.ethereum)
+      .then((user) => {
+        setUser(user);
+
+        getWill(user)
+          .then((will) => {
+            setWill(will);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+
+        if (window.ethereum !== undefined) {
+          window.ethereum.on('accountsChanged', (accounts) => {
+            if (accounts.length === 0) {
+              setUser({});
+              setWill(undefined);
+              return;
+            }
+            onConnectWallet();
+          });
+        }
+      })
+  }, []);
+
+  const onConnectWallet = function () {
+    connectWallet(window.ethereum)
+      .then((user) => {
+        setUser(user);
+
+        setLoading(true);
+        getWill(user)
+          .then((will) => {
+            setWill(will);
+          })
+          .finally(() => {
+            setLoading(false);
+          })
+      })
+  }
 
   return (
-    <div className="container">
+    <Container 
+      fluid 
+      responsive 
+      css={styles.container}
+    >
       <Head>
-        <title>Inhetherit - Claim token</title>
+        <title>Inhetherit - Create a will</title>
       </Head>
-      <main>
-        <h1>
-          Claim
-        </h1>
-      </main>
-    </div>
+      
+      <TopBar onConnectWallet={onConnectWallet} />
+      <Sidebar />
+      <Row 
+        justify="center"
+        align="center"
+      >
+        <Col 
+          justify="center" 
+          align="center" 
+          css={styles.column}
+        >
+          <Spacer y={3} />
+          TODO
+        </Col>
+      </Row>
+    </Container>
   )
 }
