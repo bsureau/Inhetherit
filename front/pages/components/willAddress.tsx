bfc8616ac34cd617f944090a80cb9f5f28f7b975
Romain Quilliot <romain.addweb@gmail.com>
2022-05-08 17:29:16 +0200

    style: reduce will address padding

--- front/pages/components/willAddress.tsx
+++ front/pages/components/willAddress.tsx
@@ -3,26 +3,26 @@ import React from 'react';
 import {Col, Link, Row, Text} from '@nextui-org/react';
 
 export default function WillAddress ({ will }) {
-    return (
-        <Col
-            css={{
-            width: "85%",
-            minWidth: "1000px",
-            margin: "auto",
-            borderRadius: "1rem",
-            background: "#ffffff",
-            }}
-        >
-            <Row css={{
-                justifyContent: "flex-start",
-                textAlign:"left",
-                padding: "1rem 3rem 1rem"
-            }}>
-                { will ?
-                    <Text css={{ display: 'block', color: 'grey'}}><strong>Your will address: </strong><Link href={`https://rinkeby.etherscan.io/address/${will.address}`} target="_blank">{will.address}</Link></Text>
-                    : ''
-                }
-            </Row>
-        </Col>
-    );
+  return (
+    <Col
+        css={{
+        width: "85%",
+        minWidth: "1000px",
+        margin: "auto",
+        borderRadius: "1rem",
+        background: "#ffffff",
+        }}
+    >
+      <Row css={{
+        justifyContent: "flex-start",
+        textAlign:"left",
+        padding: "1rem 1rem 1rem"
+      }}>
+        { will ?
+          <Text css={{ display: 'block', color: 'grey'}}><strong>Your will address: </strong><Link href={`https://rinkeby.etherscan.io/address/${will.address}`} target="_blank">{will.address}</Link></Text>
+          : ''
+        }
+        </Row>
+    </Col>
+  );
 }
