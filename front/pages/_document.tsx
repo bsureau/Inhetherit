6058f3e9fb36d23028e0c44a7132cd287025c13a
bsureau <sureau.benjamin@gmail.com>
2022-05-08 10:35:05 +0000

    feat(front): added favicon

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -14,7 +14,7 @@ class InhetheritDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/favicon.ico"/>
+          <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌈</text></svg>" /> 
           {CssBaseline.flush()}
         </Head>
         <body>
